af4d08f34a78d814542e3f0382909a53625953ad
unknown <adyashukla123@gmail.com>
2018-09-14 08:54:46 +0530

    added route

--- MyProfile/MyProfile/src/app/app.component.ts
+++ MyProfile/MyProfile/src/app/app.component.ts
@@ -20,7 +20,7 @@ import { Component } from '@angular/core';
               query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
           ]),
       ]),
-      transition('3 => 2, 2 => 1', [
+      transition('3 => 2, 3 => 1,  2 => 1 , 4 => 2, 4 => 3,4 => 1', [
           style({ height: '!' }),
           query(':enter', style({ transform: 'translateX(-100%)' })),
           query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
