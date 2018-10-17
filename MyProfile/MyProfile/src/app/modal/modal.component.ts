4022fd2d6a84efcd6ff5e8f637ced17a55360c0a
unknown <adyashukla123@gmail.com>
2018-10-17 22:11:54 +0530

    Added Validators.compose in validation

--- MyProfile/MyProfile/src/app/modal/modal.component.ts
+++ MyProfile/MyProfile/src/app/modal/modal.component.ts
@@ -20,32 +20,30 @@ export class ModalComponent implements OnInit {
     this.rForm = fb.group({
       'name': [null, Validators.required],
       'subject': [null, Validators.compose([Validators.required, Validators.maxLength(100)])],
-      'email': [null, Validators.required, Validators.email],
+      'email': [null, Validators.compose([Validators.required, Validators.email])],
       'validate': ''
     });
   }
   // Added as convenience getter for easy access to form fields
   get f() { return this.rForm.controls; }
 
-   addPost(post){
-    this.name = post.name;
-    this.subject = post.subject;
-    this.email=post.email;
-    console.log(this.rForm.value);
-   }
-  
   openModal(){
     this.display ='block'; 
  }
- onCloseHandled(){
-  this.display='none'; 
-}
-  onSubmit(){
+  onCloseHandled() {
+    this.display = 'none';
+  }
+  onSubmit() {
     this.submitted = true;
 
-    if(this.rForm.invalid){
+    if (this.rForm.invalid) {
       return;
     }
+    if (this.rForm.value["email"].includes("@")) {
+      // console.log(this.email);
+      console.log("ADya");
+    }
+    console.log(this.rForm.value);
     alert('SUCCESS')
   }
 
